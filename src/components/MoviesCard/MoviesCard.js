6d223a3a58432e204077c2e98e152224072b4276
Nasty Moiseeva <nastiakor@gmail.com>
2022-11-30 01:08:31 +0300

    Этап 4. Функциональность на React

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,16 +1,59 @@
-import image from '../../images/movies/movies_001.png'
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import baseLink from "../../utils/constants"
 
 function MoviesCard(props) {
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = (props.path === '/movies') ? props.savedMovie.some(i => (i.owner === currentUser._id && i.movieId === props.elem.id)) : props.elem.owner === currentUser._id;
+  const cardSaveButton = (
+    `card__button ${(isOwn) && `${props.path === '/movies' ? 'card__button_theme' : 'card__button_type_close'}`}`
+  );
+
+  
+  
+  const movieInfo = (props.path === '/movies') ? {
+    country: props.elem.country,
+    director: props.elem.director,
+    duration: props.elem.duration,
+    year: props.elem.year,
+    description: props.elem.description,
+    image: baseLink + props.elem.image.url,
+    trailerLink: props.elem.trailerLink,
+    nameRU: props.elem.nameRU,
+    nameEN: props.elem.nameEN,
+    thumbnail: baseLink + props.elem.thumbnail,
+    movieId: props.elem.id,
+  } : {
+      country: props.elem.country,
+      director: props.elem.director,
+      duration: props.elem.duration,
+      year: props.elem.year,
+      description: props.elem.description,
+      image: baseLink + props.elem.image,
+      trailerLink: props.elem.trailerLink,
+      nameRU: props.elem.nameRU,
+      nameEN: props.elem.nameEN,
+      thumbnail: baseLink + props.elem.thumbnail,
+      movieId: props.elem.id,
+      owner: props.elem.owner,
+      _id: props.elem._id
+  }
+
+  function handleSaveMovie() {
+    props.onSave(movieInfo); 
+  }
+
   return (
     <li className="card">
-      <img src={image} alt="Обложка" className="card__image" />
+      <a className="card__trailer" href={props.elem.trailerLink} target="_blank" rel="noreferrer" >
+        <img src={`${(props.path === '/movies') ? baseLink + props.elem.image.url : props.elem.image}`} alt="Обложка" className="card__image" />
+      </a>
       <div className="card__info">
         <div className="card__info-container">
-          <h2 className="card__title">{props.elem.title}</h2>
-          <button type='button' className={`card__button ${props.elem.active && 'card__button_theme'} ${props.saved && 'card__button_type_close'}`} aria-label=""></button>
+          <h2 className="card__title">{props.elem.nameRU}</h2>
+          <button onClick={handleSaveMovie} className={cardSaveButton} aria-label=""></button>
         </div>
-        <p className="card__duration">{props.elem.duration}</p>
-        
+        <p className="card__duration">{`${Math.floor(props.elem.duration / 60)} ч ${props.elem.duration % 60} м`}</p>
       </div>
     </li>
   )
--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,8 +1,8 @@
-function Toggle() {
+function Toggle(props) {
   return (
     <>
       <div class="toggle">
-        <input id="toggle" class="toggle__checkbox toggle-round" type="checkbox" />
+        <input id="toggle" onChange={props.toggleShortMovie} class="toggle__checkbox toggle-round" type="checkbox" checked={props.shortMovie} />
         <label for="toggle" className="toggle__label "></label>
         <label className="toggle__params">Короткометражки</label>
       </div>
